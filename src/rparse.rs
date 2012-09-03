68fbab3174bb0cc8708e23fca70c3ff204e1aeea
Jesse Jones <jesse9jones@gmail.com>
2012-09-03 14:31:47 -0700

    Export the right type names

--- src/rparse.rs
+++ src/rparse.rs
@@ -9,13 +9,14 @@ use types::*;
 export identifier, decimal_number, octal_number, hex_number, float_number, char_literal, string_literal, comment, line_comment;
 
 // parsers
-export anycp, CharParsers, 
+export ParseStatus, ParseFailed, anycp, CharParsers, 
 	match0, match1, match1_0, scan, seq2_ret_str, seq3_ret_str, seq4_ret_str, seq5_ret_str, StringParsers,
-	fails, forward_ref, or_v, ret, seq2, seq3, seq4, seq5, seq6, seq7, seq8, seq9, seq2_ret0, seq2_ret1, seq3_ret0, seq3_ret1, seq3_ret2, seq4_ret0, seq4_ret1, seq4_ret2, seq4_ret3, GenericParsers, Combinators;
+	fails, forward_ref, or_v, ret, seq2, seq3, seq4, seq5, seq6, seq7, seq8, seq9, seq2_ret0, seq2_ret1, seq3_ret0, seq3_ret1, seq3_ret2, seq4_ret0, 
+	seq4_ret1, seq4_ret2, seq4_ret3, GenericParsers, Combinators;
 
 // misc
 export EOT, is_alpha, is_digit, is_alphanum, is_print, is_whitespace;
 
 // types
-export parser, state, status, succeeded, failed;
+export Parser, State, Status, Succeeded, Failed;
 
