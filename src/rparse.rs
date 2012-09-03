6aedfc842f0f9b1968ffd7b603a3e493897d95a2
Jesse Jones <jesse9jones@gmail.com>
2012-09-03 15:23:05 -0700

    Exported optional_str

--- src/rparse.rs
+++ src/rparse.rs
@@ -12,7 +12,7 @@ export identifier, decimal_number, octal_number, hex_number, float_number, char_
 export ParseStatus, ParseFailed, anycp, CharParsers, 
 	match0, match1, match1_0, scan, seq2_ret_str, seq3_ret_str, seq4_ret_str, seq5_ret_str, StringParsers,
 	fails, forward_ref, or_v, ret, seq2, seq3, seq4, seq5, seq6, seq7, seq8, seq9, seq2_ret0, seq2_ret1, seq3_ret0, seq3_ret1, seq3_ret2, seq4_ret0, 
-	seq4_ret1, seq4_ret2, seq4_ret3, GenericParsers, Combinators;
+	seq4_ret1, seq4_ret2, seq4_ret3, GenericParsers, Combinators, optional_str;
 
 // misc
 export EOT, is_alpha, is_digit, is_alphanum, is_print, is_whitespace;
